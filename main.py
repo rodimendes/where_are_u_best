aa5035010e7c8c6c280181cf0c7f8b331846579a
Rodrigo Pinto <rodimendes@gmail.com>
2023-01-27 18:58:37 +0000

    Nome diretorio alterado

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-endpoint = "https://www.flashscore.pt/jogador/haddad-maia-beatriz/lSABz6E8/"
+endpoint = "https://en.wikipedia.org/wiki/Beatriz_Haddad_Maia#Singles:_25_(17_titles,_8_runner%E2%80%93ups)"
+# local_endpoint = "Beatriz Haddad Maia _ Player Stats & More – WTA Official.html"
 
 response = requests.get(endpoint).text
 
 soup = BeautifulSoup(response, "html.parser")
 
-print(soup.title.string)
+print(soup.text)
