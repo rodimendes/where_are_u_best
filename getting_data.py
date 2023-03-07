a8acddad49708b6f2cd2bca38db85a5c3791a508
Rodrigo Pinto <rodimendes@gmail.com>
2023-03-07 23:04:13 +0000

    Loading data into database

--- getting_data.py
+++ getting_data.py
@@ -3,33 +3,182 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 import mysql.connector
+import pandas as pd
+import pickle
+import datetime as dt
+import os
 
 
-create_statement = """
-        INSERT INTO matches
-            (player, opponent, score, W-L, date, city, surface, time, temperature, umidity, hand_player, hand_opp)
-        VALUES
-            (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    """
-
-db_config = {
-    'user': 'root',
-    'password': 'mcmlxxxiv',
-    'host': 'localhost',
-    'database': 'player_analysis_db',
-}
-
-mydb_conn = mysql.connector.connect(**db_config)
-
-
-def get_source_code(url):
+def get_source_code(url, player_name: str):
     service = Service(ChromeDriverManager().install())
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     driver = webdriver.Chrome(service=service, chrome_options=chrome_options)
     driver.get(url)
-    with open("rybakina_data.html", "w") as file:
+    with open(f"html_player_source_code/{player_name}-{dt.date.today()}.html", "w") as file:
         file.write(driver.page_source)
+
+
+def get_matches_info(source_code):
+    with open(source_code, "r") as file_to_read:
+        soup = BeautifulSoup(file_to_read, 'html.parser')
+        tournaments = soup.find_all("div", class_="player-matches__tournament")
+
+        main_player = []
+        opponents = []
+        tournaments_name = []
+        cities = []
+        matches_scores = []
+        surfaces = []
+
+
+        for tournament in tournaments:
+
+            # Tournament Name
+            tournament_name = tournament.find("a", class_="player-matches__tournament-title-link").text.strip()
+
+            # City
+            city = tournament.find("span", class_="player-matches__tournament-location").text.split(",")[0]
+
+            # Tournament Information
+            matches_info = tournament.find_all("tr", class_="player-matches__match")
+
+            # Surfaces
+            raw_opps = tournament.find_all("div", class_="player-matches__match-opponent")
+
+            for item in raw_opps:
+                if item.text.strip() != "-":
+
+                    searching_surface = tournament.find_all("div", class_="player-matches__tournament-meta-item")
+                    for surface in searching_surface:
+                        # surfaces = []
+                        surface_type = surface.text
+                        if "Surface" in surface_type:
+                            # print(surface_type.split()[1])
+                            surfaces.append(surface_type.split()[1])
+
+            for match in matches_info:
+
+                # Main player
+                player_name = soup.title.string
+                player_name = player_name.split("|")[0].strip()
+
+                # Opponent
+                raw_opponents_fname = match.find_all("span", class_="player-matches__match-opponent-first u-hide-tablet")
+                raw_opponents_lname = match.find_all("span", class_="player-matches__match-opponent-last")
+                cleaned_opponents_fname = [fname.text for fname in raw_opponents_fname]
+                cleaned_opponents_lname = [lname.text for lname in raw_opponents_lname]
+                opp_names_zip = zip(cleaned_opponents_fname, cleaned_opponents_lname)
+                opp_separated_names = list(opp_names_zip)
+                opp_names = [f"{opp[0]} {opp[1]}" for opp in opp_separated_names]
+
+                # Score
+                raw_opps = match.find_all("div", class_="player-matches__match-opponent")
+                results = []
+                for item in raw_opps:
+                    if item.text.strip() != "-":
+                        raw_scores = match.find_all("span", class_="set-score-string")
+                        match_score = ""
+                        for scores in raw_scores:
+                            if len(match_score) == 0:
+                                match_score = f"{scores.text[0]} - {scores.text[-1]}"
+                            else:
+                                match_score = f"{match_score} / {scores.text[0]} - {scores.text[-1]}"
+                        results.append(match_score)
+
+                # Results
+                raw_results = soup.find_all("td", class_="player-matches__match-cell player-matches__match-cell--winloss")
+                final_results = [result.text.strip() for result in raw_results if result.text.strip() != "-"]
+
+                for opp_name in opp_names:
+                    main_player.append(player_name)
+                    opponents.append(opp_name)
+                    tournaments_name.append(tournament_name)
+                    cities.append(city)
+                    matches_scores.append(match_score)
+
+    # Creating a dictionary
+    matches = {
+        "main_player": main_player,
+        "opponent": opponents,
+        "tournament": tournaments_name,
+        "city": cities,
+        "scores": matches_scores,
+        "result": final_results,
+        "surface": surfaces,
+    }
+
+    return matches
+
+
+def to_dataframe(player_name: str, player_matches: dict):
+    matches_df = pd.DataFrame(player_matches, columns=[column for column in player_matches.keys()])
+    with open(f"dataframes/{player_name}-{dt.date.today()}.pkl", "wb") as file:
+        pickle.dump(matches_df, file)
+    return matches_df
+
+
+def create_table(table_name):
+
+    statement = 'CREATE TABLE IF NOT EXISTS {} (id int primary key auto_increment not null, main_player varchar(30), opponent varchar(30), tournament varchar(100), city varchar(50), score varchar(50), result varchar(5), surface varchar(10));'
+
+    # connection = mysql.connector.connect(
+    #     user = os.environ.get("AWSUSER"),
+    #     password = os.environ.get("AWSPASSWORD"),
+    #     host = os.environ.get("AWSHOST"),
+    #     port = 3306,
+    #     database = os.environ.get("DATABASE")
+    # )
+
+    connection = mysql.connector.connect(
+        user = 'root',
+        password = os.environ.get("LOCALPASSWORD"),
+        host = 'localhost',
+        database = os.environ.get("LOCAL_DATABASE")
+    )
+
+    cursor = connection.cursor()
+
+    cursor.execute(statement.format(table_name))
+
+
+def load_data(dataframe):
+
+    # connection = mysql.connector.connect(
+    #     user = os.environ.get("AWSUSER"),
+    #     password = os.environ.get("AWSPASSWORD"),
+    #     host = os.environ.get("AWSHOST"),
+    #     port = 3306,
+    #     database = os.environ.get("DATABASE")
+    # )
+
+    connection = mysql.connector.connect(
+        user = 'root',
+        password = os.environ.get("LOCALPASSWORD"),
+        host = 'localhost',
+        database = os.environ.get("LOCAL_DATABASE")
+    )
+
+    # Checking if the variable dataframe is, in fact, a dataframe object to load into the database
+    if isinstance(dataframe, pd.DataFrame):
+        with connection.cursor() as cursor:
+            for index, row in dataframe.iterrows():
+                player = row["main_player"]
+                opp = row["opponent"]
+                tournament = row["tournament"]
+                city = row["city"]
+                score = row['scores']
+                result = row["result"]
+                surface = row["surface"]
+                command = f"INSERT INTO matches (main_player, opponent, tournament, city, score, result, surface) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+                cursor.execute(command, (player, opp, tournament, city, score, result, surface))
+                connection.commit()
+        print("Data uploaded successfully")
+
+    else:
+        print("Please, insert a dataframe object to load it into the database.")
+
+    return
 
 
 def get_player_name(source_code):
@@ -64,16 +213,13 @@ def get_score(source_code):
             traco = match.find("div", class_="player-matches__match-opponent").text.strip()
             if traco != "-":
                 raw_scores = match.find_all("span", class_="set-score-string")
-                matches_scores = []
-                # print(f"Tournament {pos + 1}")
-                # print(f"Match {round}")
+                placar = ""
                 for scores in raw_scores:
-                    splitted_score = scores.text.split("-")
-                    cleaned_score = []
-                    for games in splitted_score:
-                        cleaned_score.append(games[0])
-                    matches_scores.append(cleaned_score)
-                results.append(matches_scores)
+                    if len(placar) == 0:
+                       placar = f"{scores.text[0]} - {scores.text[-1]}"
+                    else:
+                        placar = f"{placar} / {scores.text[0]} - {scores.text[-1]}"
+                results.append(placar)
     return results
 
 
@@ -90,12 +236,7 @@ def get_month(source_code):
         soup = BeautifulSoup(file_to_read, 'html.parser')
     tournaments = soup.find_all("div", class_="player-matches__tournament")
     raw_event_date = [tournament.find("span", class_="player-matches__tournament-date").text.split() for tournament in tournaments]
-    # event_date = []
     month = [date[1] for date in raw_event_date]
-    # for date in raw_event_date:
-    #     month = date[1]
-    #     year = date[-1]
-    #     event_date.append([month, year])
     return month
 
 
@@ -125,10 +266,6 @@ def get_surface(source_code):
         surface_type = surface.text
         if "Surface" in surface_type:
             surfaces.append(surface_type.split()[1])
-        # if "Surface" in surface_type:
-        #     print(surface_type)
-    # for pos, surface in enumerate(raw_surfaces):
-    #     print(f"{pos} - {surface}")
     return surfaces
 
 
@@ -138,24 +275,3 @@ def get_tournament_info(source_code):
         all_tournaments = soup.find_all("a", class_="player-matches__tournament-title-link")
         tournament = [tournament.text.strip() for tournament in all_tournaments]
         return tournament
-
-
-def build_database(source_code):
-    cursor = mydb_conn.cursor()
-    cursor.execute(create_statement, (
-        get_player_name(source_code),
-        get_last_opponents(source_code),
-        get_score(source_code),
-        win_loss(source_code),
-        get_month(source_code),
-        get_year(source_code),
-        get_tournament_city(source_code),
-        get_surface(source_code),
-        time, # https://www.flashscore.pt/jogador/haddad-maia-beatriz/lSABz6E8/resultados/#s
-        temperature,
-        umidity,
-        hand_player,
-        hand_opp
-    ))
-    cursor.close()
-    mydb_conn.close()
