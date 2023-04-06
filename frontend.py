6d4684a0a5769dbb30df5861de5c8db742527080
Rodrigo Pinto <rodimendes@gmail.com>
2023-04-06 21:16:21 +0100

    Frontend

--- frontend.py
+++ frontend.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import pickle
-
+import plotly.graph_objects as go
+import plotly.express as px
+from datetime import datetime
+import pandas as pd
 
 st.title("Where are U best")
 
 table_name = st.radio("Choose the table name below:", ("matches", "tournaments"))
 
-with open("matches/daily.pkl", "rb") as database:
-    matches = pickle.load(database)
+# with open("matches/daily.pkl", "rb") as database:
+#     matches = pickle.load(database)
 
-st.dataframe(matches, height=300)
+new_matches = pd.read_pickle("matches/daily.pkl")
+
+st.dataframe(new_matches, height=300)
