62c55e43d7bc2c5676aef8c8e7046652aca19498
Rodrigo Pinto <rodimendes@gmail.com>
2023-06-03 23:45:01 +0100

    Updating matches

--- main_tasks/match_by_match.py
+++ main_tasks/match_by_match.py
@@ -256,32 +256,19 @@ def sanitizing(dataframe: pd.DataFrame):
     for pos, to_replace in enumerate(dataframe['player1']):
         if '.' in to_replace:
             for player in without_dot:
-                if to_replace[-5:] in player and to_replace[:3] == player[0]:
-                    # print(to_replace.replace(to_replace, player))
+                if to_replace.split()[-1] == player.split()[-1] and to_replace[0] == player[0]:
                     dataframe.at[pos, 'player1'] = player
-                elif to_replace[-3:] in player and to_replace[:3] == player[0]:
-                    # print(to_replace.replace(to_replace, player))
-                    dataframe.at[pos, 'player1'] = player
-
 
     for pos, to_replace in enumerate(dataframe['player2']):
         if '.' in to_replace:
             for player in without_dot:
-                if to_replace[-5:] in player and to_replace[:3] == player[0]:
-                    # print(to_replace.replace(to_replace, player))
-                    dataframe.at[pos, 'player2'] = player
-                elif to_replace[-3:] in player and to_replace[:3] == player[0]:
-                    # print(to_replace.replace(to_replace, player))
+                if to_replace.split()[-1] == player.split()[-1] and to_replace[0] == player[0]:
                     dataframe.at[pos, 'player2'] = player
 
     for pos, to_replace in enumerate(dataframe['winner']):
         if '.' in to_replace:
             for player in without_dot:
-                if to_replace[-5:] in player and to_replace[:3] == player[0]:
-                    # print(to_replace.replace(to_replace, player))
+                if to_replace.split()[-1] == player.split()[-1] and to_replace[0] == player[0]:
                     dataframe.at[pos, 'winner'] = player
-                elif to_replace[-3:] in player and to_replace[:3] == player[0]:
-                    # print(to_replace.replace(to_replace, player))
-                    dataframe.at[pos, 'winner'] = player
-
+                
     return dataframe
