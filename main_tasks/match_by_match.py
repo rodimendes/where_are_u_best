78882b816510c0ace4722f8c8bf02e8773099dbf
Rodrigo Pinto <rodimende@gmail.com>
2023-10-03 23:06:32 +0100

    testing

--- main_tasks/match_by_match.py
+++ main_tasks/match_by_match.py
@@ -31,7 +31,7 @@ def get_source_code(url):
         firefox_options.add_argument("-headless")
         driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=firefox_options)
     except:
-        print("\033[41m\033[37mFirefox Attempt FAILED\033[0m")
+        print("\033[41m\033[37mFirefox Attempt FAILED...\033[0m")
         time.sleep(2)
         print("\033[44m\033[37mChrome Attempt\033[0m")
         # Chrome web browser
